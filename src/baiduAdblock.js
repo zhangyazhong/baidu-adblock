648319cc2f4380ba65fb09dde1f0a09adf56a820
张亚中 <ZhongCentury@gmail.com>
2016-10-10 12:10:55 +0800

    显示屏蔽广告条目数
    方法：
     - 使用setInterval()轮询添加显示条目数的DOM
     - 每次执行屏蔽广告都刷新此DOM
    待解决：
     - 不优雅，效率上还有优化空间
     - console中会出现奇怪的错误

--- src/baiduAdblock.js
+++ src/baiduAdblock.js
@@ -1,14 +1,29 @@
 var adAttr = '#content_left';
 var hintAttr = '.hint_common_restop';
+var blockRecord = 0;
+var resultSummaryAttr = '.head_nums_cont_inner .nums'
+var blockSummaryAttr = '.head_nums_cont_inner .nums #blockNum'
+var blockSummaryHtml = '，<a href="https://github.com/SISSORS-FDU/baidu-adblock" style="text-decoration:none;">屏蔽百度推广</a>已为您屏蔽<span id="blockNum">' + blockRecord + '</span>个推广广告</div>';
+
+function showBlockSummary() {
+    if ($(blockSummaryAttr) === null || $(blockSummaryAttr).length < 1) {
+        $(resultSummaryAttr).append(blockSummaryHtml);
+    }
+}
 
 function block(dom) {
     $.each(dom.children(), function(i, item) {
         if ($(item).attr('id') === undefined || $(item).attr('id').length > 3) {
             $(item).remove();
+            blockRecord++;
         } else if ($(item).find("span[class='m']").text().indexOf('广告') != -1) {
             $(item).remove();
+            blockRecord++;
         }
     });
+    if ($(blockSummaryAttr) !== null && $(blockSummaryAttr).length > 0) {
+        $(blockSummaryAttr).html(blockRecord);
+    }
     dom.find(hintAttr).remove();
 }
 
@@ -23,5 +38,8 @@ document.addEventListener('DOMNodeInserted', function() {
 }, false);
 
 $(function() {
+    // if (window.localStorage.getItem('showBlockSummary') === 'true') {
+        var showBlockSummaryTimer = window.setInterval("showBlockSummary()", 1000);
+    // }
     block($(adAttr));
 });
